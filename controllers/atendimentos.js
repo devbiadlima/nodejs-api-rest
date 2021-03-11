4b95d2059ddcbfa8acb5657920be8f40d1df8b0f
Bianca Pereira <biadlima.dev@outlook.com>
2021-03-11 17:06:26 -0300

    Cadastrando um atendimento

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = (app) => {
   app.get("/atendimentos", (req, res) =>
     res.send("Você está na rota de atendimentos e está executando um GET!")
   );
+
+  app.post("/atendimentos", (req, res) => {
+    console.log(req.body);
+    res.send("Você está na rota de atendimentos e está executando um POST!");
+  });
 };
